475ba133f53f1f577c83dcee7121fc0d09949daa
hugo-lui <hugo_lui@rogers.com>
2024-06-18 16:24:59 -0400

    add clients

--- main.go
+++ main.go
@@ -1,68 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
-}
-
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ws Endpoint")
-
-	var upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-	}
-
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
-	}
-
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-	}
-
-	log.Println("Client connected")
-
-	err = ws.WriteMessage(1, []byte("HI"))
-	if err != nil {
-		log.Println(err)
-	}
-
-	reader(ws)
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
 func setupAPI() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
+	manager := newManager()
+	http.Handle("/", http.FileServer(http.Dir("./client")))
+	http.HandleFunc("/ws", manager.serveWS)
 }
 
 func main() {
 	setupAPI()
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	fmt.Println("Hello websocket")
+	log.Println("Hello websocket")
 }
---
+++ manager.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type Manager struct {
+	clients ClientList
+	sync.RWMutex
+}
+
+func newManager() *Manager {
+	return &Manager{
+		clients: make(ClientList),
+	}
+}
+
+func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
+	log.Println("Connected")
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	client := newClient(ws, m)
+	m.addClient(client)
+}
+
+func (m *Manager) addClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	m.clients[client] = true
+}
+
+func (m *Manager) removeClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	if _, ok := m.clients[client]; ok {
+		client.connection.Close()
+		delete(m.clients, client)
+	}
+}
